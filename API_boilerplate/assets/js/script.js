58d6d924444a640e7cab5aaf9895860741166193
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2023-06-20 08:29:03 +0000

    create async function

---
+++ API_boilerplate/assets/js/script.js
@@ -0,0 +1,15 @@
+const API_KEY = "n2V0EAE8LYvbNlCt9pE-R3YrFBY";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json;
+
+    
+}
